1bf2f4ea1b850787194027445bdbc47007fe1ca3
dtascone <danielletascone@Danielles-MacBook-Air.local>
2013-10-05 22:01:29 -0400

    draft one

---
+++ project1.r
@@ -0,0 +1,25 @@
+# Create two columns on single row
+par(
+  mfrow=c(1,2))
+ 
+# Create scatter plot for petals
+plot(
+  iris$Petal.Length,
+  iris$Petal.Width,
+  pch=21, # type of symbol
+  bg=c("red","green3","blue")[unclass(iris$Species)],
+  main="Petal", # main title
+  xlab="Length", # x title
+  ylab="Width" # y title
+)
+ 
+# Create scatter plot for sepal
+plot(
+  iris$Sepal.Length,
+  iris$Sepal.Width,
+  pch=21,
+  bg=c("red","green3","blue")[unclass(iris$Species)],
+  main="Sepal",
+  xlab="Length",
+  ylab="Width"
+)
